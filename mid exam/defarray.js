7862e63d9c08a986b0bdf16ff03442fe99ddb043
KainamaChristabella <69896929+KainamaChristabella@users.noreply.github.com>
2021-10-11 14:12:05 +0800

    Add files via upload

---
+++ mid exam/defarray.js
@@ -0,0 +1,26 @@
+var studentsData = [
+	{
+		id: 1,
+		NIM: 105011810001,
+		fullName: 'Andy P',
+		gender: 'Male',
+		fakultas: 'FIK',
+		study: 'Sistem Informasi',
+	},
+	{
+		id: 2,
+		NIM: 103021810001,
+		fullName: 'Clifford T',
+		gender: 'Male',
+		fakultas: 'FEB',
+		study: 'Manajemen',
+	},
+	{
+		id: 3,
+		NIM: 105021810002,
+		fullName: 'Rossa K',
+		gender: 'Female',
+		fakultas: 'FIK',
+		study: 'Informatika',
+	}
+]
